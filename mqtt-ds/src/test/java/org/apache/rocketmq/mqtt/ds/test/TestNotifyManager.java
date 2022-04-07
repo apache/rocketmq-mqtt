6bf72a5920a15dbeee59253759ee1edb1b9a2067
tom <aliensb@live.com>
2022-04-07 12:03:31 +0800

    add channel decode exception ,replace JSONObject.toJSONString(obj).getBytes(StandardCharsets.UTF_8) with JSON.toJsonByte(obj)，add some unit tests to verify these changes

--- mqtt-ds/src/test/java/org/apache/rocketmq/mqtt/ds/test/TestNotifyManager.java
+++ mqtt-ds/src/test/java/org/apache/rocketmq/mqtt/ds/test/TestNotifyManager.java
@@ -19,12 +19,15 @@
 
 package org.apache.rocketmq.mqtt.ds.test;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.commons.lang3.reflect.MethodUtils;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.mqtt.common.facade.MetaPersistManager;
+import org.apache.rocketmq.mqtt.common.model.Constants;
 import org.apache.rocketmq.mqtt.common.model.MessageEvent;
 import org.apache.rocketmq.mqtt.common.model.RpcCode;
 import org.apache.rocketmq.mqtt.ds.config.ServiceConf;
@@ -33,17 +36,23 @@ import org.apache.rocketmq.mqtt.ds.notify.NotifyManager;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 import org.apache.rocketmq.remoting.netty.NettyRemotingClient;
 import org.apache.rocketmq.remoting.protocol.RemotingCommand;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestNotifyManager {
@@ -82,9 +91,24 @@ public class TestNotifyManager {
         RemotingCommand response = mock(RemotingCommand.class);
         when(response.getCode()).thenReturn(RpcCode.SUCCESS);
         when(remotingClient.invokeSync(any(), any(), anyLong())).thenReturn(response);
-
         notifyManager.notifyMessage(new HashSet<>(Arrays.asList(new MessageEvent())));
         verify(remotingClient).invokeSync(any(), any(), anyLong());
+    }
+
+    @Test
+    public void testJsonByte() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Set<MessageEvent> messageEvents=new HashSet<>();
+        for (int i=0;i<10;i++) {
+            MessageEvent messageEvent = new MessageEvent();
+            messageEvent.setBrokerName("testBroker"+i);
+            messageEvent.setPubTopic("testTopic"+i);
+            messageEvent.setNamespace("testSpace"+i);
+            messageEvents.add(messageEvent);
+        }
+        NotifyManager notifyManager = new NotifyManager();
+        RemotingCommand remotingCommand = (RemotingCommand) MethodUtils.invokeMethod(notifyManager,  true, "createMsgEventCommand", messageEvents);
+        byte[] bytes = JSONObject.toJSONString(messageEvents).getBytes(StandardCharsets.UTF_8);
+        Assert.assertArrayEquals(remotingCommand.getBody(), bytes);
     }
 
 }

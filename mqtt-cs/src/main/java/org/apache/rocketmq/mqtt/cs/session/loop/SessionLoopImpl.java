beba2a0745785a5218625fb22c9af91d756e25c3
dongyuan.pdy <dongyuanpan0@gmail.com>
2024-05-09 11:06:05 +0800

    fix bug: PULL_EVENT not create when PULL_SUCCESS

--- mqtt-cs/src/main/java/org/apache/rocketmq/mqtt/cs/session/loop/SessionLoopImpl.java
+++ mqtt-cs/src/main/java/org/apache/rocketmq/mqtt/cs/session/loop/SessionLoopImpl.java
@@ -115,6 +115,7 @@ public class SessionLoopImpl implements SessionLoop {
 
     private AtomicLong rid = new AtomicLong();
     private long pullIntervalMillis = 10;
+    private int maxTransferCountOnMessageInDisk = 8;
 
     @PostConstruct
     public void init() {
@@ -502,7 +503,7 @@ public class SessionLoopImpl implements SessionLoop {
                 }
                 if (PullResult.PULL_SUCCESS == pullResult.getCode()) {
                     if (pullResult.getMessageList() != null &&
-                            pullResult.getMessageList().size() >= count) {
+                            pullResult.getMessageList().size() >= Math.min(count, maxTransferCountOnMessageInDisk)) {
                         scheduler.schedule(() -> pullMessage(session, subscription, queue), pullIntervalMillis, TimeUnit.MILLISECONDS);
                     }
                     boolean add = session.addSendingMessages(subscription, queue, pullResult.getMessageList());
